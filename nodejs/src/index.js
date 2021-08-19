57039592677bbeb17809a6c3ce040d96915ba6c6
Trần Thị Quỳnh Như <50972470+qnhutran1806@users.noreply.github.com>
2021-08-19 23:15:44 +0700

    'add'

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors')
 
 const app = express();
-const port = 3000;
+const port = process.env.port|| 3000;
 const database = require('./config/db/connectDB.js');
 
 //CORS
