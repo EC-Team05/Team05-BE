3968cdeddc6cf5423351835b2e9a98413a51213e
tinnguyen0812 <acquycodon1803@gmail.com>
2021-08-21 00:04:00 +0700

    update api blog

--- nodejs/src/models/blog.js
+++ nodejs/src/models/blog.js
@@ -8,6 +8,8 @@ var schema = new mongoose.Schema({
   cmt: { type: Number },
   content: { type: String },
   link: { type: String },
+  title: { type:String},
+  brief: { type:String}
 },
   {
     collection: 'BLOG'
@@ -17,6 +19,6 @@ var schema = new mongoose.Schema({
 const blog = mongoose.model('BLOG', schema);
 module.exports = {
   async all() {
-    return await blog.aggregate( [ { $project : { _id: 0, img : 1 , date : 1 , content : 1 , link : 1 } } ] );
+    return await blog.aggregate( [ { $project : { _id: 0, img : 1 , date : 1 , content : 1 , link : 1 ,title:1,brief:1} } ] );
   }
 };
